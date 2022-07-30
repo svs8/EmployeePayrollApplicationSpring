0be8553d5a7d465e00111ab44034d65ac93f5e3d
svs8 <surakshithvittalshetty@gmail.com>
2022-07-31 00:00:59 +0530

    Added additional properties

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,29 +1,27 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.ToString;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
 
     @Min(value=500,message="Min wage should be more than 500")
     public long salary;
+    public String gender;
+    @JsonFormat(pattern="dd MMM yyyy")
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> departments;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeePayrollDTO{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
