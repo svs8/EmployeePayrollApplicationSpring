37b2602ad1345a1f46e322b65a3e0488b681c30c
svs8 <surakshithvittalshetty@gmail.com>
2022-07-30 12:16:19 +0530

    Added LOMNOK Library for Logging

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,15 +1,18 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll Application Started in {} Environment", context.getEnvironment().getProperty("environment"));
     }
 
 }
