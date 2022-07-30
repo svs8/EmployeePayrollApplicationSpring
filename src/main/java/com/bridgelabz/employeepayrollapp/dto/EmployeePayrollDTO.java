60ac2c2336efe05520a3c2f40c87ea80cc2eaf1f
svs8 <surakshithvittalshetty@gmail.com>
2022-07-30 16:58:32 +0530

    Handling the exception which are thrown from the contrller

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayrollapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -9,6 +10,8 @@ public class EmployeePayrollDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
+
+    @Min(value=500,message="Min wage should be more than 500")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
